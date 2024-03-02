b30fab7508fd8395ad4e0c72018efbd8b47d1178
David Fisher <pac.dlfisher@gmail.com>
2024-03-02 09:48:42 -0500

    Create Template.ps1
    Template Generator for Functions

---
+++ Template.ps1
@@ -0,0 +1,33 @@
+<#
+  .SYNOPSIS
+    Short Description
+  .DESCRIPTION
+    Long Description
+  .EXAMPLE
+    PS C:\> <example usage>
+    Explanation of what the example does
+  .INPUTS
+    Inputs (if any)
+  .OUTPUTS
+    Outputs (if any)
+  .NOTES
+    General notes
+#>
+function Template {
+  [CmdletBinding()]
+  param (
+
+  )
+  
+  begin (
+
+  )
+  
+  process (
+
+  )
+  
+  end (
+
+  )
+}
