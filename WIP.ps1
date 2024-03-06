d930c7b9debd1fe6a391e126d189dc9e9d32dd5e
David Fisher <pac.dlfisher@gmail.com>
2024-03-06 10:02:28 -0500

    Update WIP.ps1
    Quick save
    Acquisition Agent
    Refactoring LocalAdmin to be used with local accounts
    Refactoring initialization functions for future use with workflows

--- WIP.ps1
+++ WIP.ps1
@@ -4,7 +4,7 @@ Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope CurrentUser
 # Disable Script Execution
 Set-ExecutionPolicy -ExecutionPolicy Restricted -Scope CurrentUser
 
-# TODO @phish documentation
+# TODO phish@xplatform.dev documentation
 
 function Wait-AnyKey {
   param(
@@ -50,12 +50,12 @@ function Enable-Windows {
 }
 
 # Disabled UAC Prompt
-function Unprotect-UAC {
+function Disable-UAC {
   reg.exe ADD HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System /v EnableLUA /t REG_DWORD /d 0 /f
 }
 
-function New-LocalAdmin {
-  # TODO phish TESTING!
+function New-LocalAccount {
+  # TODO phish@xplatform.dev TESTING!
   [CmdletBinding()]
   param (
     [string]
@@ -105,17 +105,22 @@ function New-LocalAdmin {
   }
 }
 
-function Unprotect-FileAndPrinter {
+function Grant-FileAndPrinterSharing {
   # Enable File and Printer Sharing (30 rules)
   netsh advfirewall firewall set rule group="File and Printer Sharing" new enable=Yes
 }
 
-function Rename-Host {
+function Grant-NetworkDiscovery {
+  # Enable Network Discovery
+  netsh advfirewall firewall set rule group="Network Discovery" new enable=Yes
+}
+
+workflow Rename-Host {
   # Saving variable for later
   $Hostname = $env:HOSTNAME
   
   # Confirm Hostname
-  while ($Confirmation -like "y*" -Or $Confirmation -like "n*") {
+  while ($Confirmation -like "y*" -Or $Confirmation -like "n*" -Or $Confirmation -eq "") {
     $Confirmation = Read-Host -Prompt "Is $Hostname the correct hostname? (Y/n)"
   }
   if ($Confirmation -like "n*") {
@@ -126,4 +131,60 @@ function Rename-Host {
 
 function Disable-DefaultAdmin {
   Disable-LocalUser -Name "Administrator"
+}
+
+# TODO phish@xplatform.dev chrome password manager https://www.tenforums.com/tutorials/115669-enable-disable-saving-passwords-google-chrome-windows.html
+
+function New-AcquisitionAgentTask {  
+  <#
+  $action = New-ScheduledTaskAction -Execute "C:\Program Files (x86)\Acquisition Agent\Acquisition Agent.exe"
+  $trigger = New-ScheduledTaskTrigger -AtLogon
+  $principal = New-ScheduledTaskPrincipal -UserID "$env:HOSTNAME\Administrator" 
+  $settings = New-ScheduledTaskSettingsSet -AtLogOn
+  $task = New-ScheduledTask -Action $action -Principal $principal -Trigger $trigger -Settings $settings
+  Register-ScheduledTask T1 -InputObject $task
+  #>
+
+  $AA_Path = "C:\Program Files (x86)\Acquisition Agent\Acquisition Agent.exe"
+  $Action = New-ScheduledTaskAction -Execute $AA_Path
+  $Description = "Runs $AA_Path as admin"
+  # TODO phish@xplatform.dev Principal mapping
+  $Principal = New-ScheduledTaskPrincipal -GroupId "BUILTIN\Administrators" -RunLevel Highest
+  # TODO phish@xplatform.dev Setting mapping
+  $Settings = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries
+  # TODO phish@xplatform.dev Verify -AtLogOn for all users
+  $Trigger = New-ScheduledTaskTrigger -AtLogOn
+  $Task = New-ScheduledTask -Action $Action -Description $Description -Principal $Principal -Settings $Settings
+  Register-ScheduledTask "Run Acquisition Agent as Admin" -InputObject $Task
+}
+
+function Initilize-Cleanup {
+  Unprotect-LocalPasswordLimit
+  Enable-Windows
+  Disable-UAC
+  New-LocalAccount
+  Grant-FileAndPrinterSharing
+  Grant-NetworkDiscovery
+  Disable-DefaultAdmin
+}
+
+function Initilize-PC {
+  Write-Host "If this was invoked directly, you may have meant to run 'Initilize-LocalPC' or 'Initilize-DomainPC'"
+  Unprotect-LocalPasswordLimit
+  Enable-Windows
+  Disable-UAC
+  New-LocalAccount
+  Grant-FileAndPrinterSharing
+  New-AcquisitionAgentTask
+  Disable-DefaultAdmin
+}
+
+# TODO phish@xplatform.dev implement this
+workflow Initilize-LocalPC {
+  
+}
+
+# TODO phish@xplatform.dev implement this
+workflow Initilize-DomainPC {
+
 }
