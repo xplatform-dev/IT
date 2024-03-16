2f4008350f6d13957f16af22bf29f51e7ff188ac
David Fisher <pac.dlfisher@gmail.com>
2024-03-16 15:55:32 -0400

    Update WIP.ps1
    Fixed errors associated with older powershell.
    Removed erroneous "function"
    Began work on renaming PC

--- WIP.ps1
+++ WIP.ps1
@@ -31,8 +31,6 @@ function Disable-UAC {
   reg.exe ADD HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System /v EnableLUA /t REG_DWORD /d 0 /f
 }
 
-function 
-
 function New-LocalAccount {
   # TODO phish@xplatform.dev TESTING!
   [CmdletBinding()]
@@ -41,7 +39,8 @@ function New-LocalAccount {
     [string]$UserName,
 
     [Parameter(Mandatory=$true)]
-    [ValidateSet("Users", "Administrators", ErrorMessage="Probably need to edit the script for that group")]$GroupName,
+    [ValidateSet("Users", "Administrators"
+      <# PowerShell 7 Feature, ErrorMessage="Probably need to edit the script for that group" #>)]$GroupName,
 
     [Parameter(Mandatory=$false)]
     [string]$Description,
@@ -94,37 +93,13 @@ function Grant-NetworkDiscovery {
   netsh advfirewall firewall set rule group="Network Discovery" new enable=Yes
 }
 
-<# FIXME phish@xplatform.dev
-workflow Rename-Host {
-  # Saving variable for later
-  $Hostname = $env:HOSTNAME
-  
-  # Confirm Hostname
-  while ($Confirmation -like "y*" -Or $Confirmation -like "n*" -Or $Confirmation -eq "") {
-    $Confirmation = Read-Host -Prompt "Is $Hostname the correct hostname? (Y/n)"
-  }
-  if ($Confirmation -like "n*") {
-    $Hostname = Read-Host -Prompt "New Hostname"
-    Rename-Computer -NewName $Hostname
-  }
-} #>
-
 function Disable-DefaultAdmin {
   Disable-LocalUser -Name "Administrator"
 }
 
 # TODO phish@xplatform.dev chrome password manager https://www.tenforums.com/tutorials/115669-enable-disable-saving-passwords-google-chrome-windows.html
 
-function New-AcquisitionAgentTask {  
-  <#
-  $action = New-ScheduledTaskAction -Execute "C:\Program Files (x86)\Acquisition Agent\Acquisition Agent.exe"
-  $trigger = New-ScheduledTaskTrigger -AtLogon
-  $principal = New-ScheduledTaskPrincipal -UserID "$env:HOSTNAME\Administrator" 
-  $settings = New-ScheduledTaskSettingsSet -AtLogOn
-  $task = New-ScheduledTask -Action $action -Principal $principal -Trigger $trigger -Settings $settings
-  Register-ScheduledTask T1 -InputObject $task
-  #>
-  
+function New-AcquisitionAgentTask {
   $AA_Path = "C:\Program Files (x86)\Acquisition Agent\Acquisition Agent.exe"
   
   $TaskName = "Run Acquisition Agent as Admin"
@@ -160,17 +135,54 @@ function Initialize-PC {
   Enable-Windows
   Disable-UAC
   Unprotect-LocalPasswordLimit
-  New-LocalAccount -UserName "ITAdmin" -GroupName "Administrators" -Description "42 North Dental Local Administrator Account" -FullName "IT Admin"
   Grant-FileAndPrinterSharing
   New-AcquisitionAgentTask
   # Rename-Computer
   # Restart-Computer -Wait 
+  New-LocalAccount -UserName "ITAdmin" -GroupName "Administrators" -Description "42 North Dental Local Administrator Account" -FullName "IT Admin"
   New-LocalAccount -UserName "Administrator" -GroupName "Administrators" -Description "Windows Default Admin Account" -FullName "Local Administrator"
   # Install Applications
   # Local User Account?
   # Group Policy
   Disable-DefaultAdmin
   # Restart-Computer -Wait
+}
+
+function Display-NamingConvention {
+  Write-Host "Naming convention is 000-XXX#-T"
+  Write-Host "\tWhere '000' = Location ID number, HQ is 000"
+  Write-Host "\tWehre 'XXX' = Department shorthand:"
+  Write-Host "\t\t\tFD = Front Desk,\t\t# = 1 OR 2 digit(s) for the stations"
+  Write-Host "\t\t\tOP = Operatory,\t\tThe # = TWO DIGITS for the operatory room. OP04, even if the office only has 8 rooms"
+  Write-Host "\t\t\tDR = Doctor or Associate,\t\t# = 1 digit for the stations"
+  Write-Host "\t\t\tIT = IT Closet,\t\t# = TBD"
+  Write-Host "\t\t\tLAB = Laboratory,\t\t# = TBD"
+  Write-Host "\t\t\tMAN = Manager,\t\tNo number"
+  Write-Host "\t\t\tDIR = Director,\t\tNo number"
+  Write-Host "\t\t\tMKT = Markering,\t\t# = TBD"
+  Write-Host "\t\t\tACT = Accounting,\t\t# = TBD"
+  Write-Host "\t\t\tetc."
+  Write-Host "\tWhere 'T' is the type shorthand:"
+  Write-Host "\t\t\tD = Desktop"
+  Write-Host "\t\t\tL = Laptop"
+  <# We don't use these 
+  Write-Host "\t\t\tT = Tablet"
+  Write-Host "\t\t\tP = Phone"
+  <# #>
+  Write-Host "Examples:"
+  Write-Host "000-IT04-L = Headquarters, IT Department, Computer #04, laptop"
+  Write-Host "001-FD01-D = GD Brookline, Front Desk, Computer #01, Desktop"
+}
+
+function Rename-Host {
+  $Input = $null
+  do {
+    Write-Host "The current computer name is $env:ComputerName, do you want to rename it?"
+    $Input = Read-Host -Prompt "[Y] Yes, [N] No (Default), [I] Information, [A] Abort
+  } until ($Input.ToUpper()[0] -match "YNIA")
+  switch ($Input) {
+    # FIXME phish@xplatform.dev Finish this.
+  }
 }
 
 Initialize-PC
